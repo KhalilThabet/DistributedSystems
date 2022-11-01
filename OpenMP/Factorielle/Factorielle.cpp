9f714d9b0ffa5e6e4d810649f840f57032a58f75
khalilthabet <khalil.thabet@ensi-uma.tn>
2022-11-01 17:31:29 +0100

    Adding_Reduction

--- OpenMP/Factorielle/Factorielle.cpp
+++ OpenMP/Factorielle/Factorielle.cpp
@@ -5,7 +5,8 @@
 using namespace std::chrono;
 using namespace std;
 int main(){
-    int N =25; //25
+    int N =15; //25
+    for (int k=0;k<10;k++){
     long long a = 1;
     auto start = high_resolution_clock::now();
 
@@ -15,21 +16,21 @@ int main(){
     auto stop = high_resolution_clock::now();
     auto duration = duration_cast<microseconds>(stop - start);
  
-    cout << "Time taken to sequential algorithm: "
-         << duration.count() << " microseconds" << endl;
-    cout<<a<<endl;
+    cout << "|"
+         << duration.count() << " us ";
+    // cout<<a<<endl;
     a=1;
     start = high_resolution_clock::now();
-    #pragma omp parallel for schedule (guided, 1)
+    #pragma omp parallel for reduction(*:a)
         for(int idx=1;idx<=N;idx++){
             a*=idx;
         }
     stop = high_resolution_clock::now();
     duration = duration_cast<microseconds>(stop - start);
- 
-    cout << "Time taken to parallel algorithm: "
-         << duration.count() << " microseconds" << endl;
-    cout<<a<<endl;
-
+    cout << "|"
+         << duration.count() << " us|" << endl;
+    // cout<<a<<endl;
+    }
+    
     return 0;
 }
